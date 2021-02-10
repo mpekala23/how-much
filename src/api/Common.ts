64d3e0e6380470f2688dca1b37956e8a5b3a1c7c
Mark Pekala <mpekala@college.harvard.edu>
2021-02-10 01:41:13 -0500

    stupid error

--- src/api/Common.ts
+++ src/api/Common.ts
@@ -1,5 +1,4 @@
 export const dummy = 1;
 
 export const API_URL =
-  //"http://ec2-13-59-35-124.us-east-2.compute.amazonaws.com:8080/";
-  "http://localhost:8080/";
+  "http://ec2-13-59-35-124.us-east-2.compute.amazonaws.com:8080/";
