00df35fb372f181c3b5e5ac0b17be9fef0af17eb
Mark Pekala <mpekala@college.harvard.edu>
2021-02-14 22:37:07 -0500

    Update

--- src/pages/Information/Information.react.tsx
+++ src/pages/Information/Information.react.tsx
@@ -20,7 +20,62 @@ const InformationPage: React.FC = () => {
             color: "#005940",
           }}
         >
-          A next generation digital status symbol for the world.
+          A next generation digital status symbol for the world. This is simply
+          the ability to access a page that says, "Thanks for X", where X is the
+          amount in $USD that the you provide on the checkout page. This page
+          also contains buttons that make it easy to share your accomplishment
+          on social media.
+        </p>
+        <p
+          style={{
+            fontSize: "36pt",
+            color: "#005940",
+          }}
+        >
+          Transaction Currency
+        </p>
+        <p
+          style={{
+            fontSize: "24pt",
+            color: "#005940",
+          }}
+        >
+          $USD
+        </p>
+        <p
+          style={{
+            fontSize: "36pt",
+            color: "#005940",
+          }}
+        >
+          Returns / refunds
+        </p>
+        <p
+          style={{
+            fontSize: "24pt",
+            color: "#005940",
+          }}
+        >
+          We do not offer any returns or refunds.
+        </p>
+        <p
+          style={{
+            fontSize: "36pt",
+            color: "#005940",
+          }}
+        >
+          Privacy Policy
+        </p>
+        <p
+          style={{
+            fontSize: "24pt",
+            color: "#005940",
+          }}
+        >
+          No user personal information will be held or shared by BS Holding
+          Company, LLC. All payment information will be proceeded through Stripe
+          Inc. Users can direct their questions to
+          bsholdingcompanyllc@gmail.com.
         </p>
         <Link to="/">
           <p
